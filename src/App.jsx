3e186933357276e08c460e6d8e31e124dc5589bc
Abdulraman Saumu <abdulramansaumu@gmail.com>
2025-07-12 22:04:34 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+
+
+function App() {
+    return (
+        <>
+            <WelcomeMessage />
+        </>
+    );
+}
+
+function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
